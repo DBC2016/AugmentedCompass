309c51438471843199979e8ed643a0e9c832def0
DBC2016 <childers2032@gmail.com>
2016-05-23 13:39:51 -0400

    Initial Commit

---
+++ AugmentedCompass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AugmentedCompass
+//
+//  Created by Demond Childers on 5/23/16.
+//  Copyright © 2016 Demond Childers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
